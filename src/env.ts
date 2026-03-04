2c1c363b8bebe24c55809911bff473eadb9d15eb
Mohammed Tamimi <Mohammed.Tamimi@iztechvalley.ps>
2026-03-04 20:39:19 +0200

    fix: added the deployed url

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,6 @@
 export const env = {
-  API_BASE_URL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000',
+  API_BASE_URL:
+    import.meta.env.VITE_API_BASE_URL || 'https://shelfie-backend-production.up.railway.app',
   ENVIRONMENT: import.meta.env.VITE_ENVIRONMENT || 'development',
   IS_DEV: import.meta.env.VITE_ENVIRONMENT === 'development',
 }
